54e88be7bbfbd5b4cba69f12db520ca0133d8b9f
Bruno <brunoowap@gmail.com>
2023-01-04 12:44:22 -0300

    Adicionando os tests para o hover e adicinando código do hover

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -12,7 +12,7 @@ test("Condições iniciais", () => {
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
   expect(confirmButton).toBeDisabled();
 });
-/* User Event sempre retorna uma promise, logo a função é assincrona */
+/* User Event sempre retorna uma promise, logo a função é assincrona  */
 test("Checkbox ativar button no primeiro click e desabilitar no segundo", async () => {
   const user = userEvent.setup();
   render(<SummaryForm />);
@@ -26,4 +26,24 @@ test("Checkbox ativar button no primeiro click e desabilitar no segundo", async 
   expect(confirmButton).toBeEnabled();
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responde quando acontecer um hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+  /* Primeiro testar se o popover iniciar "escondido" */
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  /* Segundo testar se o popover aparece quando tiver hover */
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  /* Terceiro testar se o popover some quando o mouse sai */
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
