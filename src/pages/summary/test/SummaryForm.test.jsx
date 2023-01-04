330402cf407649625b50f9aa1060e0a94637fda7
Bruno <brunoowap@gmail.com>
2023-01-04 12:06:27 -0300

    trocando o fireevent para o userEvent

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
-
+import userEvent from "@testing-library/user-event";
 /* Testar se o checkbox começa não checked e o button desabilitado */
 test("Condições iniciais", () => {
   render(<SummaryForm />);
@@ -12,8 +12,9 @@ test("Condições iniciais", () => {
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
   expect(confirmButton).toBeDisabled();
 });
-
-test("Checkbox ativar button no primeiro click e desabilitar no segundo", () => {
+/* User Event sempre retorna uma promise, logo a função é assincrona */
+test("Checkbox ativar button no primeiro click e desabilitar no segundo", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
@@ -21,8 +22,8 @@ test("Checkbox ativar button no primeiro click e desabilitar no segundo", () => 
 
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
